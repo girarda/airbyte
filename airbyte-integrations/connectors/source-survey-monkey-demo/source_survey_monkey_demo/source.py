e6ae61911b5eff85200ed5eb989f9e31531b184a
Alexandre Girard <alexandre@airbyte.io>
2024-03-30 06:36:37 -0700

    check works

--- airbyte-integrations/connectors/source-survey-monkey-demo/source_survey_monkey_demo/source.py
+++ airbyte-integrations/connectors/source-survey-monkey-demo/source_survey_monkey_demo/source.py
@@ -12,6 +12,12 @@ from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.auth import Oauth2Authenticator, TokenAuthenticator
+from airbyte_cdk.sources.streams.availability_strategy import AvailabilityStrategy
+from requests import HTTPError, codes, exceptions  # type: ignore[import]
+from airbyte_cdk.sources.streams.availability_strategy import AvailabilityStrategy
+from airbyte_cdk.models import FailureType, SyncMode, AirbyteMessage
+from requests import HTTPError, codes, exceptions  # type: ignore[import]
+from airbyte_cdk.utils.traced_exception import AirbyteTracedException
 
 _PAGE_SIZE = 1000
 
@@ -44,8 +50,15 @@ class SurveyMonkeyBaseStream(HttpStream, ABC):
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         response_json = response.json()
-        if self._data_field:
-            yield from response_json.get(self._data_field, [])
+        # https://api.surveymonkey.com/v3/docs?shell#error-codes
+        if response_json.get("error") in (1010, 1011, 1012, 1013, 1014, 1015, 1016, 1017, 1018):
+            internal_message = "Unauthorized credentials. Response: {response_json}"
+            external_message = "Can not get metadata with unauthorized credentials. Try to re-authenticate in source settings."
+            raise AirbyteTracedException(
+                message=external_message, internal_message=internal_message, failure_type=FailureType.config_error
+            )
+        elif self._data_field:
+            yield from response_json[self._data_field]
         else:
             yield from response_json
 
@@ -66,11 +79,32 @@ class SurveyMonkeyBaseStream(HttpStream, ABC):
     def primary_key(self) -> Optional[Union[str, List[str], List[List[str]]]]:
         return self._primary_key
 
+    @property
+    def availability_strategy(self) -> Optional[AvailabilityStrategy]:
+        return None
+
 
 # Source
 class SourceSurveyMonkeyDemo(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
-        return True, None
+        first_stream = next(iter(self.streams(config)))
+
+        stream_slice = next(iter(first_stream.stream_slices(sync_mode=SyncMode.full_refresh)))
+
+        try:
+            read_stream = first_stream.read_records(sync_mode=SyncMode.full_refresh, stream_slice=stream_slice)
+            first_record = None
+            while not first_record:
+                first_record = next(read_stream)
+                if isinstance(first_record, AirbyteMessage):
+                    if first_record.type == "RECORD":
+                        first_record = first_record.record
+                        return True, None
+                    else:
+                        first_record = None
+            return True, None
+        except Exception as e:
+            return False, f"Unable to connect to the API with the provided credentials - {str(e)}"
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         auth = TokenAuthenticator(token=config["access_token"])
